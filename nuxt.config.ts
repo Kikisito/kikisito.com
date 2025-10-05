8aa430e0c270588193a6d525310154219bccffae
Kikisito <bekikisito@gmail.com>
2025-10-05 22:21:15 +0200

    Transición entre páginas

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,13 @@ export default defineNuxtConfig({
   
   css: ['~/assets/css/main.css'],
 
+  app: {
+    pageTransition: {
+      name: 'page',
+      mode: 'out-in'
+    }
+  },
+
   content: {
     watch: {
       enabled: true
